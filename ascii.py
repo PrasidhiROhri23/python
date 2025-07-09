44d63d18c505094b9982fdadc18deccfe374bed3
Prasidhi R Ohri <prasidhiohrri@gmail.com>
2025-07-09 16:52:48 +0530

    Reformed

--- ascii.py
+++
@@ -1,2 +0,0 @@
-choice = input("Enter a letter: ");
-print("ASCII value of " + choice + " is", ord(choice));
--- wordCount.py
+++
@@ -1,6 +0,0 @@
-user = input("Enter a sentence: ");
-user = user+ ' ';
-words = user.split();
-count = len(words);
-
-print(f'The number of words in the given sentence are {count}');
